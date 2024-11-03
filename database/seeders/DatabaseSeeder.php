c27809c794c8ebbd1ed39062d36d6611b691ce4d
Lorenz Adam Damara <lorenzrenz@gmail.com>
2024-11-03 14:53:44 +0700

    change seeder for user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
         Sensor::factory(100)->create();
         Device::factory(25)->create();
